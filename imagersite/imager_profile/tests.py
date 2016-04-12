9104ca84cc38426de7aa853c87dd716a98a8d398
Michael Stokley <michael@michaelstokley.com>
2016-04-11 17:14:57 -0700

    profile, user models linked, tested

--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -2,9 +2,19 @@ from django.test import TestCase
 from .models import ImagerProfile
 from django.contrib.auth.models import User
 
+
 # Create your tests here.
 class ImagerProfileModel(TestCase):
 
     def test_profile_created_for_user(self):
+        """
+        Instantiating a new user object instantiates and links
+        a new user record object
+        """
         new_user = User.objects.create_user(username='bob')
-        self.assertEquals(new_user.profile, new_user.profile.user)
+        self.assertEquals(new_user, new_user.profile.user)
+
+    def test_dunder_str(self):
+        new_user = User.objects.create_user(username='bob')
+        profile = new_user.profile
+        self.assertEquals(p)
